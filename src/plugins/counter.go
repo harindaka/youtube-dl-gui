0f1eda49fce2de9d0342e32b34b52a841064df1a
harindaka <harindaka@gmail.com>
2017-12-09 01:58:44 +0530

    partial implementation of debug server

---
+++ src/plugins/counter.go
@@ -0,0 +1,17 @@
+package plugins
+
+//Counter plugin
+type Counter struct {
+	value uint
+}
+
+//NewCounter creates a new Counter plugin
+func NewCounter() Counter {
+	return Counter{
+		value: 0,
+	}
+}
+
+func (c *Counter) add(value uint) {
+	c.value += value
+}
