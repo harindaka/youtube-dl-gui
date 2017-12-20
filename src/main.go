78ac609caca025780054be4192cb929d74e03801
harindaka <harindaka@gmail.com>
2017-12-20 11:45:05 +0530

    finalized goToJs and jsToGo bridging methods

--- src/main.go
+++ src/main.go
@@ -23,7 +23,7 @@ func main() {
 	registerMessageHandlers(goui)
 
 	goui.DevServerPort = 3030
-	goui.Start(StartModeApplication, registerAssets)
+	goui.Start(StartModeDevServer, registerAssets)
 }
 
 func registerAssets(goui *GoUI) {
